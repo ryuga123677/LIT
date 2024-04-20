1083274b8bea30c27da8d82d18cee533d3c4c9ba
Shubhamrane-890 <shubhamrane885@gmail.com>
2024-04-21 00:16:51 +0530

    update female 2

---
+++ src/pages/Female2.js
@@ -0,0 +1,301 @@
+import React, { useEffect, useRef, useState } from "react";
+import p5 from "p5";
+import { useNavigate } from "react-router-dom";
+import imgPath1 from "../common/assets/Avatar/femalechar/Female AI croqui 2.png";
+import shirt1 from "../common/assets/Avatar/femalechar/shirts/f_shirt.png";
+import shirt2 from "../common/assets/Avatar/femalechar/shirts/f_shirt2.png";
+import shirt3 from "../common/assets/Avatar/femalechar/shirts/f_shirt3.png";
+import shirt4 from "../common/assets/Avatar/femalechar/shirts/f_shirt4.png";
+import shirt5 from "../common/assets/Avatar/femalechar/shirts/f_shirt5.png";
+import shirt6 from "../common/assets/Avatar/femalechar/shirts/f_shirt6.png";
+//imported shirts images
+import pant1 from "../common/assets/Avatar/femalechar/pants/f_pant.png";
+import pant2 from "../common/assets/Avatar/femalechar/pants/f_pant2.png";
+import pant3 from "../common/assets/Avatar/femalechar/pants/pant3.png";
+import pant4 from "../common/assets/Avatar/femalechar/pants/f_pant4.png";
+import pant5 from "../common/assets/Avatar/femalechar/pants/f_pant5.png";
+import pant6 from "../common/assets/Avatar/femalechar/pants/f_pant6.png";
+//imported pant images
+import bg from "../common/assets/bg.jpg";
+//imported background image
+const Female2 = () => {
+  const navigate = useNavigate();
+  const canvasRef = useRef(null);
+  const [shirtImage, setShirtImage] = useState(shirt4); //starting or initial cloth
+  const [pantImage, setPantImage] = useState(pant4);
+  const [value1, setValue1] = useState(50);
+  const [value2, setValue2] = useState(50);
+  const [value3, setValue3] = useState(50);
+  const [value4, setValue4] = useState(50); //setvalues for sliders
+
+  const [shirtPosition, setShirtPosition] = useState({ x: 192, y: 105 }); //starting or initial cloth positioning values
+  const [shirtScale, setShirtScale] = useState({ width: 205, height: 158 });
+
+  const [pantPosition, setPantPosition] = useState({ x: 127, y: 206 });
+  const [pantScale, setPantScale] = useState({ width: 369, height: 389 });
+
+  const imgRef = useRef(null);
+  const shirtref1 = useRef(null);
+  const shirtref2 = useRef(null);
+  const shirtref3 = useRef(null);
+  const shirtref4 = useRef(null);
+  const shirtref5 = useRef(null);
+  const shirtref6 = useRef(null);
+
+  const pantref1 = useRef(null);
+  const pantref2 = useRef(null);
+  const pantref3 = useRef(null);
+  const pantref4 = useRef(null);
+  const pantref5 = useRef(null);
+  const pantref6 = useRef(null);
+
+  const background = useRef(null);
+
+  const myP5Ref = useRef(null);
+  // using slider so to get a position value for cloth!!
+  const handleSliderChange1 = (event) => {
+    setValue1(event.target.value);
+    setPantPosition((prevState) => ({ ...prevState, x: event.target.value })); //for pant setPantposition
+  };
+
+  const handleSliderChange2 = (event) => {
+    setValue2(event.target.value);
+    setPantPosition((prevState) => ({ ...prevState, y: event.target.value }));
+  };
+
+  const handleSliderChange3 = (event) => {
+    setValue3(event.target.value);
+    setPantScale((prevState) => ({ ...prevState, width: event.target.value }));
+  };
+
+  const handleSliderChange4 = (event) => {
+    setValue4(event.target.value);
+    setPantScale((prevState) => ({
+      ...prevState,
+      height: event.target.value,
+    }));
+  };
+
+  useEffect(() => {
+    const preloadImage = (p) => {
+      try {
+        imgRef.current = p.loadImage(imgPath1);
+        shirtref1.current = p.loadImage(shirtImage);
+        pantref1.current = p.loadImage(pantImage);
+        background.current = p.loadImage(bg);
+      } catch (error) {
+        console.error("Error loading image:", error);
+      }
+    };
+
+    const setupSketch = (p) => {
+      try {
+        p.setup = () => {
+          const canvasWidth = canvasRef.current.offsetWidth;
+          const canvasHeight = canvasRef.current.offsetHeight;
+          p.createCanvas(canvasWidth, canvasHeight);
+        };
+        p.draw = () => {
+          p.background(background.current);
+          p.image(imgRef.current, 89, 20, 450, 640);
+          p.image(
+            shirtref1.current,
+            shirtPosition.x,
+            shirtPosition.y,
+            shirtScale.width,
+            shirtScale.height
+          );
+          p.image(
+            pantref1.current,
+            pantPosition.x,
+            pantPosition.y,
+            pantScale.width,
+            pantScale.height
+          );
+        };
+      } catch (error) {
+        console.error("Error setting up sketch:", error);
+      }
+    };
+
+    try {
+      myP5Ref.current = new p5(setupSketch, canvasRef.current);
+      preloadImage(myP5Ref.current);
+    } catch (error) {
+      console.error("Error initializing p5.js sketch:", error);
+    }
+
+    return () => {
+      try {
+        myP5Ref.current.remove();
+      } catch (error) {
+        console.error("Error removing p5.js sketch:", error);
+      }
+    };
+  }, [
+    shirtImage,
+    shirtPosition,
+    shirtScale,
+    pantImage,
+    pantPosition,
+    pantScale,
+    value1,
+    value2,
+    value3,
+    value4,
+  ]);
+
+  const handleChangeshirt = (
+    newShirtImage,
+    newX,
+    newY,
+    newWidth,
+    newHeight
+  ) => {
+    setShirtImage(newShirtImage);
+    setShirtPosition({ x: newX, y: newY });
+    setShirtScale({ width: newWidth, height: newHeight });
+  };
+  const handleChangepant = (newPantImage, newX, newY, newWidth, newHeight) => {
+    setPantImage(newPantImage);
+    setPantPosition({ x: newX, y: newY });
+    setPantScale({ width: newWidth, height: newHeight });
+  };
+  const handleChangelip = (newPantImage, newX, newY, newWidth, newHeight) => {
+    //setLipImage(newPantImage);
+    //setLipPosition({ x: newX, y: newY });
+    //setLipScale({ width: newWidth, height: newHeight });
+  };
+  return (
+    <>
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          gap: 20,
+          flexWrap: "wrap",
+          height: "100vh",
+        }}
+      >
+        <div ref={canvasRef} style={{ width: "50%" }}></div>
+        <div style={{ display: "flex", flexDirection: "column" }}>
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              overflowX: "auto",
+              maxWidth: "400px",
+            }}
+          >
+            <button
+              onClick={() => handleChangeshirt(shirt1, 215, 102, 198, 204)} //buttoms for shirts
+            >
+              <img src={shirt1} height="100px" width="100px" />
+            </button>
+            <button
+              onClick={() => handleChangeshirt(shirt2, 217, 98, 194, 268)}
+            >
+              <img src={shirt2} height="100px" width="100px" />
+            </button>
+            <button
+              onClick={() => handleChangeshirt(shirt3, 163, 77, 245, 285)}
+            >
+              <img src={shirt3} height="100px" width="100px" />
+            </button>
+            <button
+              onClick={() => handleChangeshirt(shirt4, 192, 105, 205, 158)}
+            >
+              <img src={shirt4} height="100px" width="100px" />
+            </button>
+            <button
+              onClick={() => handleChangeshirt(shirt5, 176, 66, 251, 248)}
+            >
+              <img src={shirt5} height="100px" width="100px" />
+            </button>
+            <button
+              onClick={() => handleChangeshirt(shirt6, 189, 109, 224, 165)}
+            >
+              <img src={shirt6} height="100px" width="100px" />
+            </button>
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "row",
+              overflowX: "auto",
+              maxWidth: "400px",
+            }}
+          >
+            <button onClick={() => handleChangepant(pant1, 133, 263, 359, 326)}>
+              <img src={pant1} height="100px" width="100px" />
+            </button>
+            <button onClick={() => handleChangepant(pant2, 149, 248, 325, 388)}>
+              <img src={pant2} height="100px" width="100px" />
+            </button>
+            <button onClick={() => handleChangepant(pant3, 189, 240, 259, 336)}>
+              <img src={pant3} height="100px" width="100px" />
+            </button>
+            <button onClick={() => handleChangepant(pant4, 127, 206, 369, 389)}>
+              <img src={pant4} height="100px" width="100px" />
+            </button>
+            <button onClick={() => handleChangepant(pant5, 201, 239, 205, 225)}>
+              <img src={pant5} height="100px" width="100px" />
+            </button>
+            <button onClick={() => handleChangepant(pant6, 74, 244, 458, 405)}>
+              <img src={pant6} height="100px" width="100px" />
+            </button>
+          </div>
+          <div>
+            <div>
+              <label>Slider 1</label>
+              <input
+                type="range"
+                min="0"
+                max="700"
+                value={value1}
+                onChange={handleSliderChange1}
+              />
+              <span>{value1}</span>
+            </div>
+            <div>
+              <label>Slider 2</label>
+              <input
+                type="range"
+                min="0"
+                max="700"
+                value={value2}
+                onChange={handleSliderChange2}
+              />
+              <span>{value2}</span>
+            </div>
+            <div>
+              <label>Slider 3</label>
+              <input
+                type="range"
+                min="0"
+                max="700"
+                value={value3}
+                onChange={handleSliderChange3}
+              />
+              <span>{value3}</span>
+            </div>
+            <div>
+              <label>Slider 4</label>
+              <input
+                type="range"
+                min="0"
+                max="700"
+                value={value4}
+                onChange={handleSliderChange4}
+              />
+              <span>{value4}</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Female2;
