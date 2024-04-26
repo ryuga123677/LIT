5d7b8da0ff76d2dc96c773c2756e807e1890bcfb
ryuga123677 <kishanss311@gmail.com>
2024-04-26 17:35:45 +0530

    improved avatar selection

--- src/pages/AvatarSelection.js
+++ src/pages/AvatarSelection.js
@@ -10,25 +10,31 @@ import maleImg3 from "../common/assets/Avatar/malechar/Male R 03.png";
 import femaleImg1 from "../common/assets/Avatar/femalechar/Female AI croqui 01.png";
 import femaleImg2 from "../common/assets/Avatar/femalechar/Female AI croqui 2.png";
 import femaleImg3 from "../common/assets/Avatar/femalechar/Female AI croqui 03.png";
+import {
+  useMediaQuery,
+} from "@mui/material";
 
 // AvatarSelection component
 const AvatarSelection = () => {
 
   // Hook for navigation
   const navigate = useNavigate();
+  const isXsScreen = useMediaQuery("(max-width:600px)");
 
   
   return (
     <>
-      <div>chooose charater</div>
-      <div
+      
+      {!isXsScreen && (<div
         style={{
           display: "flex",
           flexDirection: "column",
           justifyContent: "center",
+        
           alignItems: "center",
           height: "max-height",
           width: "100%",
+          
         }}
       >
         <div
@@ -94,7 +100,63 @@ const AvatarSelection = () => {
           />
 
         </div>
-      </div>
+      </div>)}
+      {isXsScreen && (<div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+        
+          alignItems: "center",
+          height: "max-height",
+          width: "100%",
+          
+        }}
+      >
+       
+          <img
+            onClick={() => navigate("/menavatar1")}
+            src={maleImg1}
+            alt="Male Avatar 1"
+            style={{ width: "33%", height: "100%" }}
+          />
+          <img
+            onClick={() => navigate("/menavatar2")}
+            src={maleImg2}
+            alt="Male Avatar 2"
+            style={{ width: "33%", height: "100%" }}
+          />
+
+          <img
+            onClick={() => navigate("/menavatar3")}
+            src={maleImg3}
+            alt="Male Avatar 3"
+            style={{ width: "33%", height: "100%", cursor: "pointer" }}
+          />
+        
+   
+          <img
+            src={femaleImg1}
+            onClick={() => navigate("/femaleavatar1")}
+            alt="Female Avatar 1"
+            style={{ width: "33%", height: "100%" }}
+          />
+          <img
+            src={femaleImg2}
+            onClick={() => navigate("/female2")}
+            alt="Female Avatar 2"
+            style={{ width: "33%", height: "100%" }}
+          />
+
+          <img
+            onClick={() => navigate("/femaleavatar3")}
+            src={femaleImg3}
+            alt="Female Avatar 3"
+            style={{ width: "33%", height: "100%", cursor: "pointer" }}
+          />
+
+    
+      </div>)}
     </>
   );
 };
